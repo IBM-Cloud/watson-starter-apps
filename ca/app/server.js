3468bbab547fff3a919547ff13507dbe065a22c4
German Attanasio <germanattanasio@gmail.com>
2016-12-08 14:27:29 -0500

    Use  PORT instead of VCAP_APP_PORT, fixes #3

--- ca/app/server.js
+++ ca/app/server.js
@@ -2,6 +2,5 @@
 'use strict';
 
 var server = require('./app');
-var port = process.env.PORT || process.env.VCAP_APP_PORT || port;
-var port = process.env.VCAP_APP_PORT || 3000;
+var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
 server.listen(port);
